082a8700ba7a4cc3653fa4b8a5c3b734057ecf48
innTall <avg999.trade@gmail.com>
2025-10-02 20:21:07 +0200

    small commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.23gvf6p40fg"
+    "revision": "0.hot02evl6k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
