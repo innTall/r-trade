d1aca5c6f4ad7cb7f79e8327a809ab2a394c25b1
innTall <avg999.trade@gmail.com>
2025-10-02 14:30:53 +0200

    new .env file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.n25m2h578g8"
+    "revision": "0.57kc7lu3gc8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
